3c83443b89a2d728ba8208cf95f0c87273cc7206
Nhanhaiho <80114104+Nhanhaiho@users.noreply.github.com>
2026-03-09 12:06:31 +0700

    Add Streamlit app update and requirements

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from src.predict import predict_sentiment
 
 st.title("Vietnamese Sentiment Analysis")
 
-text = st.text_area("Đồ ăn ngon thế")
+text = st.text_area("Đồ ăn ok phết, nhưng mà phục vụ chán quá, không có tâm gì cả. Mình sẽ không quay lại đây nữa đâu.")
 
 if st.button("Predict"):
     result = predict_sentiment(text)
